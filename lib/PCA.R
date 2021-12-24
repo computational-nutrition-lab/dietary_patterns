d560ed0fa3fad47887dc32f154681a119386074b
Rie Sadohara <56049845+RieSadohara@users.noreply.github.com>
2021-12-24 17:09:34 +0900

    PCA updated, salt data added

--- lib/PCA.R
+++ lib/PCA.R
@@ -7,84 +7,96 @@
 # Created on 12.17.2021 by Rie Sadohara
 # ========================================================================================
 
-# ========================================================================================
-# Set working directory 
-# ========================================================================================
+# ---------------------------------------------------------------------------------------------------------------
+# Function to subset specific data from the totals.
 
-# Set your working directory as to the main directory.
-# Session --> Set working directory --> Choose directory.
-
-# Name your main directory for future use. 
-  main.wd <- file.path(getwd())
-
-# Import source code to run the analyses to follow.
-  source("lib/load_and_check.R")
-
-# Load example data files =============================================================== 
-# Specify the directory where the data is.
-  SpecifyDataDirectory(directory.name = "eg_data/dietstudy/")
-
-# Load the totals data.
-  totals <- read.table("Totals_to_use.txt",  sep = "\t", header = T)
-
- 
-# Function to subset nutrition data (columns "PROT" through "B12_ADD").
-
-SubsetNutrients <- function(data = totals, start_col = "PROT", end_col = "B12_ADD"){
-
-  # Column Variables of "totals" dataframe.
-  colvars <- names(totals)
-  
-  # Get the first ID
-  start_col <- match(start_col, colvars)
-  
-  # Get the second ID
-  end_col <- match(end_col, colvars)
-  
-  # Subset range
-  nutrients <<- totals[, start_col:end_col]
-  
-  # Print what was loaded.
-  cat("nutrients dataset contains the following", length(colnames(nutrients)), "columns.", "\n")
-  print(colnames(nutrients))
+  SubsetColumns <- function(data, start.col, end.col){
+    # Column Variables of "totals" dataframe.
+    colvars <- names(data)
+    # Get the first ID
+    start.col <- match(start.col, colvars)
+    # Get the second ID
+    end.col <- match(end.col, colvars)
+    # Subset range
+    subsetted <<- data[, start.col:end.col]
+    # Print what was loaded.
+    cat("'subsetted' contains the following", length(colnames(subsetted)), "columns.", "\n")
+    print(colnames(subsetted))
   }
+# ---------------------------------------------------------------------------------------------------------------
+
+# Keep only the columns with non-zero variance in order to perform PCA.
+  KeepNonZeroVarColumns <- function(data = subsetted){
+    subsetted_non0var <<- subsetted[, which(apply(subsetted, 2, var) != 0)] 
+      # Print which column(s) were removed.
+      if(ncol(subsetted) == ncol(subsetted_non0var)){
+        cat("No columns were removed.", "\n")
+      }
+      if(ncol(subsetted) != ncol(subsetted_non0var)){
+        cat("The following column(s) in ", deparse(substitute(data)), " had zero variance and were removed.", "\n")
+        print(which(apply(subsetted, 2, var) == 0))
+      }
+    }
   
-  
-# The columns "PROT" and "B12_ADD" and all columns in between will be selected. 
-#   Edit the start_col and/or end_col if different from the default.  
-  SubsetNutrients(data = totals, start_col = "PROT", end_col = "B12_ADD")  
+   
+# ---------------------------------------------------------------------------------------------------------------
+# Function to create a scree plot.
 
-# Perform PCA with all the nutrients, scaled.
-  scaled_pca <- prcomp(nutrients, scale = T) 
-
-  summary(scaled_pca)
-
-# Calculate the variance explained for each PC.
-  var_explained_df <- data.frame(PC = rep(1:length(colnames(nutrients))),
-                               var_explained = (scaled_pca$sdev)^2/sum((scaled_pca$sdev)^2))
-
-# Subset the first 10 PCs
-  first10PCs <- var_explained_df[1:10, ]
-
-# Create a scree plot.
-  require(ggplot2)
-  ggplot(first10PCs, aes(x=PC, y=var_explained*100)) + 
-    geom_line() + 
-    geom_point() +
-    theme_bw(base_size = 15) +
-    labs(x = "Number of PCs",
-         y = "Variance explained by PCs (%)") +
-    theme(panel.grid.major = element_blank()) +
-    theme(panel.grid.minor = element_blank()) +
-    theme(aspect.ratio = 0.9)
-  
-
-
+  LineScreePlot <- function(pca.result = scaled_pca){
+    # Calculate the variance explained for each PC.
+    var_explained_df <- data.frame(PC = rep(1:length(colnames(subsetted_non0var))),
+                                   var_explained = (pca.result$sdev)^2/sum((pca.result$sdev)^2))
+    # Subset the first 10 PCs
+    first10PCs <- var_explained_df[1:10, ]
+    # Create a scree plot.
+    require(ggplot2)
+    ggplot(first10PCs, aes(x = PC, y = var_explained*100)) + 
+      geom_line() + 
+      geom_point() +
+      scale_x_continuous(breaks = 1:10) +
+      labs(x = "Number of PCs",
+           y = "Variance explained by PCs (%)") +
+      theme(panel.grid.major = element_blank()) +
+      theme(panel.grid.minor = element_blank()) +
+      theme(axis.title.x = element_text(margin=margin(t = 10, r = 0, b = 0, l = 0) ) ) +
+      theme(axis.title.y = element_text(margin=margin(t = 0, r = 10, b = 0, l = 0) ) ) +
+      theme(aspect.ratio = 0.9)
+  }
+# ---------------------------------------------------------------------------------------------------------------
 
   
+# ---------------------------------------------------------------------------------------------------------------
+# Function to create a biplot with the individuals as black dots.
+  BiplotDots <- function(pca.result = scaled_pca, pca.data = subsetted_non0var){
+    require(ggfortify) # Need ggfortify packge to use 'autoplot'.
+    autoplot(object = pca.result, data = pca.data,
+             loadings = T, loadings.label = T, loadings.colour = 'pink',
+             loadings.label.size=3) +
+      theme(panel.grid.major = element_blank()) +
+      theme(panel.grid.minor = element_blank()) +
+      theme(axis.title.x = element_text(margin=margin(t = 10, r = 0, b = 0, l = 0) ) ) +
+      theme(axis.title.y = element_text(margin=margin(t = 0, r = 10, b = 0, l = 0) ) ) +
+      theme(aspect.ratio = 1)
+  } 
+
+# ---------------------------------------------------------------------------------------------------------------
   
+# ---------------------------------------------------------------------------------------------------------------
+# Function to create a biplot with the individuals labeled.
+  BiplotLabeled <- function(pca.result = scaled_pca, pca.data = subsetted_non0var, individuals.label = TRUE){
+    require(ggfortify)
+    autoplot(object = pca.result, data = pca.data, 
+             label = individuals.label, label.size = 3, shape =FALSE,  
+             loadings = T, loadings.label = T, loadings.colour = 'pink',
+             loadings.label.size=3) +
+      theme(panel.grid.major = element_blank()) +
+      theme(panel.grid.minor = element_blank()) +
+      theme(axis.title.x = element_text(margin=margin(t = 10, r = 0, b = 0, l = 0) ) ) +
+      theme(axis.title.y = element_text(margin=margin(t = 0, r = 10, b = 0, l = 0) ) ) +
+      theme(aspect.ratio = 1)
+  } 
   
-  
+# ---------------------------------------------------------------------------------------------------------------
   
   
   
--- users/03_PCA.R
+++ users/03_PCA.R
@@ -1,7 +1,7 @@
 # For USERS ==============================================================================
 
 # ========================================================================================
-# Visualize the mean values of %kcal from carbohydrate, protein, and total fat.
+# PCA analysis on nutrients.
 # Version 1
 # Created on 12.16.2021 by Rie Sadohara
 # ========================================================================================
@@ -31,17 +31,18 @@
 
 # Import source code to run the analyses to follow.
   source("lib/load_and_check.R")
-  source("lib/percent_kcal.R")
+  source("lib/PCA.R")
 
 # Load example totals data =============================================================== 
 # Specify the directory where the data is.
   SpecifyDataDirectory(directory.name = "eg_data/dietstudy/")
+  # SpecifyDataDirectory(directory.name = "eg_data/salt/")
   
 # Load the totals.csv
   totals <- read.table("Totals_to_use.txt",  sep = "\t", header = T)
   
   # If totals data is a csv:
-  # totals <- read.csv("Totals.csv", sep = ",", header = T)
+  # totals <- read.csv(list.files(pattern = '\\Totals.csv$'))
   
 # Come back to the main directory
   setwd(main.wd)
@@ -49,6 +50,33 @@
 # ========================================================================================  
 # 
 # ========================================================================================
-  ggplot2::theme_set(theme_bw(14))
+# Specify the plot theme and base font size to use.
+  library(ggplot2)
+  ggplot2::theme_set(theme_bw(base_size = 14))
 
 # Subset nutrition data.
+# The columns specified as start.col, end.col, and all columns in between will be selected.
+# Nutrient analysis --> start.col = "PROT",    end.col = "B12_ADD"
+# Nutrient analysis --> start.col = "F_TOTAL", end.col = "A_DRINKS"
+  SubsetColumns(data = totals, start.col = "F_TOTAL", end.col = "A_DRINKS")  
+
+# Pick up only the columns with non-zero variance, in order to do PCA.
+# The removed columns will be shown if any.
+  KeepNonZeroVarColumns(data = subsetted)
+
+# Perform PCA with all the nutrients, scaled.
+  scaled_pca <- prcomp(x = subsetted_non0var, scale = T)   
+
+# Create a scree plot.
+  LineScreePlot(pca.result = scaled_pca)
+
+# Create a biplot.
+  # A biplot with the individuals as black dots.
+  BiplotDots(   pca.result = scaled_pca, pca.data = subsetted_non0var)
+  
+  # A biplot with the individuals labeled.
+  BiplotLabeled(pca.result = scaled_pca, pca.data = subsetted_non0var, individuals.label = TRUE)
+  
+#
+
+  
