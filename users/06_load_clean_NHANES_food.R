eab73d3d303a3401756d7fe3237ec3af0df3e5d8
Rie Sadohara <56049845+RieSadohara@users.noreply.github.com>
2022-04-28 18:53:51 +0900

    NHANES take average

--- users/06_load_clean_NHANES_food.R
+++ users/06_load_clean_NHANES_food.R
@@ -76,8 +76,8 @@
 
 # Remove children.  
   # Load the demographics file, then filter by age > 18.
-  demo <- read.xport("eg_data/NHANES/DEMO_I.XPT")
-  adults <- demo[demo$RIDAGEYR >= 18, ]
+  demog <- read.xport("eg_data/NHANES/DEMO_I.XPT")
+  adults <- demog[demog$RIDAGEYR >= 18, ]
   
 # Retain those with complete data (STZ==1)
   # DR1DRSTZ == 1: reliable and all relevant variables associated with the 24-hour dietary recall contain a value.
@@ -85,102 +85,119 @@
   food1 <- subset(Food_D1, DR1DRSTZ == 1)
   food2 <- subset(Food_D2, DR2DRSTZ == 1)
   
-# Create a vector of SEQN of those who reported more than 1 food item per day.
+  food1names <- unique(food1$SEQN)
+  food2names <- unique(food2$SEQN)
+  keepnames <- food1names[food1names %in% food2names]
+  keepnames_adults <- keepnames[keepnames %in% adults$SEQN]
+  
+# Keep those who reported more than 1 food item per day.
   freqtable1 <- as.data.frame(table(food1$SEQN))
   freqtable1_m <- freqtable1[freqtable1$Freq > 1, ]
   colnames(freqtable1_m)[1] <- "SEQN"
   keepnames_adults_mult1 <- keepnames_adults[keepnames_adults %in% freqtable1_m$SEQN]
   # Take only those in keepnames_adults_mult1.
-  food1 <- food1[food1$SEQN %in% keepnames_adults_mult1, ] 
+  food1b <- food1[food1$SEQN %in% keepnames_adults_mult1, ] 
   
   # Do the same for food2
   freqtable2 <- as.data.frame(table(food2$SEQN))
   freqtable2_m <- freqtable2[freqtable2$Freq > 1, ]
   colnames(freqtable2_m)[1] <- "SEQN"
   keepnames_adults_mult2 <- keepnames_adults[keepnames_adults %in% freqtable2_m$SEQN]
-  food2 <- food2[food2$SEQN %in% keepnames_adults_mult2, ] 
+  food2b <- food2[food2$SEQN %in% keepnames_adults_mult2, ] 
 
 # Create a vector of SEQN of those that have both day 1 and day 2 data.
-  food1names <- unique(food1$SEQN)
-  food2names <- unique(food2$SEQN)
-  keepnames12 <- food1names[food1names %in% food2names]
+  food1bnames <- unique(food1b$SEQN)
+  food2bnames <- unique(food2b$SEQN)
+  keepnames12 <- food1bnames[food1bnames %in% food2bnames]
 
 # Combine day 1 and day 2 data.
   # Day 1
   # Import the list of variables present in Day 1. 
   day1variables <- read.table('eg_data/NHANES/NHANES_Food_VarNames_Day1.txt', header=F)
   # Which variables to pick up from the food data
-  var_to_use1 <- names(food1) %in% day1variables$V1
+  var_to_use1 <- names(food1b) %in% day1variables$V1
   # pick up only the specified variables 
-  food1 <- food1[, var_to_use1]
+  food1c <- food1b[, var_to_use1]
   # Remove "DR1T", "DR1" from the column names 
-  colnames(food1) <- gsub(colnames(food1), pattern = "DR1I", replacement = "")
-  colnames(food1) <- gsub(colnames(food1), pattern = "DR1", replacement = "")
+  colnames(food1c) <- gsub(colnames(food1c), pattern = "DR1I", replacement = "")
+  colnames(food1c) <- gsub(colnames(food1c), pattern = "DR1", replacement = "")
   # Check
-  head(food1, 1)
+  head(food1c, 1)
  
   # Do the same for Day 2  
   day2variables <- read.table('eg_data/NHANES/NHANES_Food_VarNames_Day2.txt', header=F)
-  var_to_use2 <- names(food2) %in% day2variables$V1
-  food2 <- food2[, var_to_use2]
-  colnames(food2) <- gsub(colnames(food2), pattern = "DR2I", replacement = "")
-  colnames(food2) <- gsub(colnames(food2), pattern = "DR2", replacement = "")
-  head(food2, 1)
+  var_to_use2 <- names(food2b) %in% day2variables$V1
+  food2c <- food2b[, var_to_use2]
+  colnames(food2c) <- gsub(colnames(food2c), pattern = "DR2I", replacement = "")
+  colnames(food2c) <- gsub(colnames(food2c), pattern = "DR2", replacement = "")
+  head(food2c, 1)
   
   # Do the columns of food1 and food2 match?
-  identical(colnames(food1), colnames(food2))
+  identical(colnames(food1c), colnames(food2c))
 
   # Make a day variable before combining
-  food1$Day = 1
-  food2$Day = 2
+  food1c$Day = 1
+  food2c$Day = 2
   
   # Combine food1 and food2
-  food12 <- rbind(food1, food2)
+  food12c <- rbind(food1c, food2c)
   
-  # Pick up only those in keepnames12.
-  food12 <- food12[food12$SEQN %in% keepnames12, ]
+# Pick up only those in keepnames12.
+  food12d <- food12c[food12c$SEQN %in% keepnames12, ]
+
+    # Some checking -- to be removed.  
+      length(unique(food12d$SEQN))
+      sum(table(unique(food12d$SEQN))[,1] <4) 
+      sum(table(food12d$SEQN, food12d$Day)[,1] <2) # how many has <2 food entries? Should be zero. 
+      sum(table(food12d$SEQN, food12d$Day)[,2] <2) # how many has <2 food entries? Should be zero.
+      aaa = as.data.frame(table(food12d$SEQN, food12d$Day))
+      head(aaa, 10)
+      table(aaa$Var2)
+      tail(aaa[order(aaa$Freq), ], 10)
+      subset(food12d, SEQN==86563)[, 'Day'] # This one reported only 1 food/day. Should be nonexistent.
+
+# You may also want to consider special diets that some participants are following: e.g. DASH diet, diabetic diet, etc.
+# Depending on your research question, you may want to exclude those following special diets.
+# The diet information is found in totals day 1.  
+
+# ===============================================================================================================
+# Calculate totals/day/participant with the food data of the selected SEQNs.
+# ===============================================================================================================
+
+# Take only the Day 1 data
+  food12d_d1 <- subset(food12d, Day==1) 
+
+# Sum nutrients.        
+  total1 <- aggregate(food12d_d1[, 2:66], by=list(food12d_d1$SEQN), FUN=sum)
+  total1$Day <- 1
+  colnames(total1)[1] <- "SEQN"
+
+# Create a vector of number of food items reported by each participant.
+  n_items1 <- as.data.frame(table(food12d_d1$SEQN))
+  colnames(n_items1) <- c("SEQN", "NoOfItems")
+  # Add it to total1
+  total1b <- merge(x=total1, y=n_items1, by="SEQN", all.x=T)
+  
+      # Some checking
+      subset(total1b, NoOfItems<2)
+      # Look for any missing data
+      total1b[is.na(total1b$NoOfItems), ]
+  
+# Do the same thing for Day 2.  
+      
+  ###### RESUME FROM HERE ###########################
+
+      
+      
+      
+      
   
   
-  sum(table(unique(food12$SEQN))[,1] <4) 
-  sum(table(food12$SEQN, food12$Day)[,1] <2)
-  sum(table(food12$SEQN, food12$Day)[,2] <2)
-  aaa = as.data.frame(table(food12$SEQN, food12$Day))
-  head(aaa[order(aaa$Freq), ], 10)
-  table(food12$Day)
   
-  subset(food12, SEQN==86563)[, 'Day']
   
-  ######### STILL, SEQN THAT ONLY HAS DAY 1 DATA IS PICKED UP. WHY??????????????????
+  
   
   #
-  # dietary pattern/special diet map
-  library(dplyr)
-  diet_type_map <- nhanes_totals_1 %>% select(SEQN, DRQSDIET, DRQSDT1, DRQSDT2, DRQSDT3, DRQSDT4, DRQSDT5, DRQSDT6, DRQSDT7, DRQSDT8, DRQSDT9, DRQSDT10, DRQSDT11, DRQSDT12, DRQSDT91)
-  diet_type_map <- diet_type_map[diet_type_map$SEQN %in% keepnames12, ]
-   ############ THERE IS NO MATCH BETWEEN TOTALS$SEQN AND KEEPNAMES12????????????????????
-  
-  head(diet_type_map)
-  head(keepnames12)
-  tail(keepnames12)
-  head(nhanes_totals_1$SEQN)
-  tail(nhanes_totals_1$SEQN)
-  head(Food_D1$SEQN)
-  tail(Food_D1$SEQN)
-  
-  # food:  83732 - 93702 
-  # total: 93704 - 102956
-  
-  
-  
-  
-  
-  
-  
-  #
-  
-  
-  
-  
   
   
   
@@ -211,6 +228,9 @@
   # Outliers won't severely affect main analysis conclusions (ASA24 data cleaning doc)
   # But it's always a good idea to take a look at the distributions of variables of interest. 
   # Could calculate totals by occasion, similar to ASA24 code.
+  
+
+  
   
   
   
