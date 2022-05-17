862741cb18916fe57f0f191e9b17ff4bc0c2d7f3
Rie Sadohara <56049845+RieSadohara@users.noreply.github.com>
2022-05-17 18:13:33 +0900

    PCA with different diet groups

--- users/NHANES special diet.R
+++ users/NHANES special diet.R
@@ -58,9 +58,11 @@
   table(metadata$DRQSDIET)
   # 682 people.
 
+  library(dplyr)
 # Extract only those following any specific diet.
-  metadata_1 <- metadata %>% filter(DRQSDIET == 1) 
-  # metadata_1 <- metadata %>% filter(DRQSDIET == 2)  # for checking.
+  # metadata_1 <- metadata %>% filter(DRQSDIET == 1) 
+  metadata_1 <- metadata %>% filter(DRQSDIET == 2)  # for checking.
+# How many are on each diet?
   table(metadata_1$DRQSDT1) 
   table(metadata_1$DRQSDT2) 
   table(metadata_1$DRQSDT3) 
@@ -76,59 +78,150 @@
   table(metadata_1$DRQSDT91) 
 
   # Create a vector to use in a table to save count results. 
-  diets <- c("DRQSDT1","DRQSDT2","DRQSDT3","DRQSDT4","DRQSDT5","DRQSDT6","DRQSDT7","DRQSDT8","DRQSDT9","DRQSDT10","DRQSDT11","DRQSDT12","DRQSDT91")
+  dietsvec <- c("DRQSDT1","DRQSDT2","DRQSDT3","DRQSDT4","DRQSDT5","DRQSDT6","DRQSDT7","DRQSDT8","DRQSDT9","DRQSDT10","DRQSDT11","DRQSDT12","DRQSDT91")
   
   # Create a dataframe to count results.
-  dietfreq = data.frame(diet=diets, Yes=c(345, 94, 97, 37, 1, 7, 124, 14, 58, 15, 5, 5, 17))
+  dietfreq = data.frame(diet=dietsvec, Yes=c(345, 94, 97, 37, 1, 7, 124, 14, 58, 15, 5, 5, 17))
   sum(dietfreq$Yes)
   # 819 > 682. So, some people are on more than 1 diet. 
-  
-# Extract SEQN and diet variables only. 
-  metadata_2 <- metadata_1[, c("SEQN", diets)]
-  head(metadata_2)
-  
-# Count the number of variables that are not NA. If > 1, they are on multiple diets.
-  metadata_2$count_na <- rowSums(is.na(metadata_2))
-  head(metadata_2)
+
+# Count the number of diet variables that are not NA. If 13, they are not on a specific diet. If 12, they are on 1 diet. If < 12, they are on multiple diets.
+  metadata$count_na <- rowSums(is.na(metadata[, dietsvec])) # count number of NA's in the columns specified in "dietsvec".
+  table(metadata$count_na)
   
 # Compute the number of diets they are on.
-  metadata_2$n_diets <- 13 - metadata_2$count_na
-  table(metadata_2$n_diets)
-  sum(metadata_2$n_diets) # Should be the same as sum(dietfreq$Yes).
+  metadata$n_diets <- 13 - metadata$count_na
+  table(metadata$n_diets)
   # So, 575 people are on 1 diet.
+  sum(  metadata$n_diets) # Should be the same as sum(dietfreq$Yes).
   
-# Select only those following 1 diet.
-  metadata_3 <- subset(metadata_2, n_diets==1)
-  head(metadata_3)
-  table(metadata_3$n_diets)  # OK
-  table(metadata_3$count_na) # OK
-  
-# Find the diet they are on.
-# Calc rowSums for the diet columns.
-  dietcode <- rowSums(metadata_3[, 2:14], na.rm = T) 
-  head(dietcode, 10)
-
-# Append the dietcode.
-  metadata_4 <- cbind(metadata_3, dietcode)
-  head(metadata_4)
-  table(metadata_4$dietcode)
-  
-# Load the dietcode and dietname table
+# -----------------------------------------------------------------------------------------------------------------
+# Append the name of the diet to metadata and freqtable.
+# Load the dietcode and dietname reference table.
   diettable <- read.table("E:/MSU OneDrive 20210829/UMinn/20_NHANES/2015-16/Data/NHANES_Totals_VarNames_Dietnames.txt", sep="\t", header=T)
   # Create the number only dietcode...
-  diettable$dietcode_nonly <- c(1,2,3,4,5,6,7,8,9,10, 11, 12, 91)
+  diettable$dietcode_nonly <- c(1,2,3,4,5,6,7,8,9,10,11,12,91)
   # Change the columnnames, to be able to do merge by dietcode.
   colnames(diettable) <- c("dietcode_full", "dietname", "dietcode")
   
-# Merge (append the dietname)
-  metadata_5 <- merge(x=metadata_4, y=diettable, by="dietcode", all.x=T)
-  head(metadata_5)
-
-# The frequency of people on each diet!!
-  as.data.frame(table(metadata_5$dietname))
-  # Weight loss, diabetic, low salt are the top 3.   
-   
   
+# Select only those following 1 diet. -------------------------------------------------------------------
+  metadata_a <- subset(metadata, n_diets==1)
+  dim(metadata_a)
+  head(metadata_a)
+  table(metadata_a$n_diets)  # OK
+  table(metadata_a$count_na) # OK
+  
+# Find the diet they are on. 
+  # Calc rowSums for the diet columns. (because only the columns of the diet that each individual is on has a number, not NA.)
+  metadata_a$dietcode <- rowSums(metadata_a[, dietsvec], na.rm = T) 
+  # Check
+  head(metadata_a[, c("SEQN", dietsvec, "dietcode")], 3)
+  # freq table of diets followed by them.
+  onedietfreq <- as.data.frame(table(metadata_a$dietcode))
+  colnames(onedietfreq)[1] <- "dietcode"
+  onedietfreq
+
+# Append the dietname to the frequency table as well. 
+  onedietfreq_name <- merge(x=onedietfreq, y=diettable, by="dietcode", all.x=T)
+  sum(onedietfreq_name$Freq) # Should total to the number of individuals following one diet. 
+  onedietfreq_name
+# Weight loss, diabetic, low salt are the top 3.   
+
+# Add the dietname to metadata.
+  metadata_1diet <- merge(x=metadata_a, y=diettable, by="dietcode", all.x=T)
+  head(metadata_1diet,1)
+
+# metadata_1diet has the metadata of the individuals who are in QCtotal and following one specific diet.
+  # Extract only necessary info.
+  metadata_b <- metadata_1diet[, c("SEQN", "dietcode", "dietname")]
+  head(metadata_b)
+  
+# Take the first 20 individuals from weight-loss (low calorie; n=300), diabetic (n=97), 
+  # low-salt (n=51), low-fat (n=43), and low-carb (n=33)
+  weightloss <- subset(metadata_1diet, dietname == "Weight_loss")[1:20, "SEQN"]   
+  diabetic <- subset(metadata_1diet, dietname   == "Diabetic")[1:20, "SEQN"]   
+  lowsalt <- subset(metadata_1diet, dietname    == "Low_salt")[1:20, "SEQN"]   
+  lowfat <- subset(metadata_1diet, dietname     == "Low_fat")[1:20, "SEQN"]   
+  lowcarb <- subset(metadata_1diet, dietname    == "Low_carb")[1:20, "SEQN"]   
+  
+  regulardiet <- subset(metadata, DRQSDIET == 2)[1:20, "SEQN"] # those are not following any specific diet.
+  
+# Combine those
+  diffdiet120_short <- data.frame(Weight_loss=weightloss, 
+                            Diabetic=diabetic, 
+                            Low_salt=lowsalt, 
+                            Low_fat=lowfat, 
+                            Low_carb=lowcarb, 
+                            Regular=regulardiet)
+  diffdiet120_short
+  
+# Create a long table out of this.
+  library(reshape2)
+  diffdiet120 <- melt(diffdiet120_short)
+  colnames(diffdiet120) <- c("Diet", "SEQN")
+  head(diffdiet120)
+  
+# ---------------------------------------------------------------------------------------------------
+# Add diet info while picking up the individuals from totalQC.
+  QCtotal_1diet <- merge(x=diffdiet120, y=QCtotal, by="SEQN", all.x=T)
+  head(QCtotal_1diet, 2)
+
+# Save it as an input file.
+  write.table(QCtotal_1diet, "eg_data/NHANES/NHANES1516_total_d12_FC_mean_QC_2_120diffdiet.txt",
+              sep="\t", quote=F)
+  
+# Let's see how different thoese diet groups are .....  
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=KCAL)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=TFAT)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=PROT)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=CARB)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=FIBE)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=PF_TOTAL)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=V_TOTAL)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=V_STARCHY_OTHER)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=V_LEGUMES)) + geom_boxplot()
+  ggplot(QCtotal_1diet, aes(x=factor(Diet), y=D_TOTAL)) + geom_boxplot()
+# Not super different???
+         
+# ---------------------------------------------------------------------------------------------------
+# Define the sample total data with which you are going to do clustering. 
+  totals_QCed_sampled <- QCtotal_1diet
+
+# Follow the code in prep_for_clustering.    
+
+#### Clustering analysis ####  
+
+# Plot PC1 and PC2 and color-code individuals by their diet.
+# Load the FC results
+  PCA_FC <- read.table("results/PCA_results/NHANES1516_totalsbyhand_FC_n120/NHANES1516_total_d12_FC_mean_QC_2_120diffdiet_input_PCs.txt",
+                       sep="\t", header=T)
+  head(PCA_FC)
+
+# Plot PC1 and PC2 of the PCA results of Foof Categories.  
+  fillcolor = c("darkred", "orange", "darkgreen", "darkblue", "violet", "grey45")
+  colcolor = c("darkred", "orange", "darkgreen", "darkblue", "violet", "grey45")
+  ggplot(data=PCA_FC, aes(x=PC1, y=PC2, fill=Diet, color=Diet, shape=Diet))+
+    geom_point(size=3) + 
+    scale_fill_manual(values=fillcolor) +
+    scale_color_manual(values=colcolor) +
+    theme(legend.position = "bottom")
+
+# Load the nutrient results
+  PCA_nut <- read.table("results/PCA_results/NHANES1516_totalsbyhand_nut_n120/NHANES1516_total_d12_nut_mean_QC_2_120diffdiet_input_PCs.txt",
+                       sep="\t", header=T)
+  head(PCA_nut)
+  
+  # Plot PC1 and PC2 of the PCA results of Foof Categories.  
+  fillcolor = c("darkred", "orange", "darkgreen", "darkblue", "violet", "grey45")
+  colcolor = c("darkred", "orange", "darkgreen", "darkblue", "violet", "grey45")
+  ggplot(data=PCA_nut, aes(x=PC1, y=PC2, fill=Diet, color=Diet, shape=Diet))+
+    geom_point(size=3) + 
+    scale_fill_manual(values=fillcolor) +
+    scale_color_manual(values=colcolor) +
+    theme(legend.position = "bottom")
+  
+  #  
   
   
   
